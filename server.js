2340bcaf3eb3561443a830f78ea51699b8dc806c
Luis (LT) Carbonell <lt.carbonell@gmail.com>
2017-01-11 19:01:05 -0500

    Read team standings from databases and display to homepage

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-var express    = require("express");
+// set up ========================
+var express  = require('express');
+var app      = express();                               // create our app w/ express
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 var mysql      = require('mysql');
+
+
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -16,14 +21,39 @@ connection.connect(function(err){
   }
 });
 
-app.get("/",function(req,res){
-  connection.query('SELECT * from standings', function(err, rows, fields) {
-    connection.end();
-    if (!err)
-    console.log('The solution is: ', rows);
-    else
-    console.log('Error while performing Query.');
+/* Routes */
+app.get('/api/standings', function(req, res) {
+  console.log("Api touched");
+  connection.query('SELECT * FROM standings ORDER BY points DESC', function(err, rows, fields) {
+    if (!err) {
+      console.log('The solution is: ', rows);
+      res.json(rows);
+    }
+    else {
+      throw err;
+    }
   });
+})
+
+app.get('/api/standings', function(req, res) {
+  console.log("Api touched");
+  connection.query('SELECT * FROM standings ORDER BY points DESC', function(err, rows, fields) {
+    if (!err) {
+      res.json(rows);
+    }
+    else {
+      throw err;
+    }
+  });
+})
+
+app.get("",function(req,res){
+  res.sendFile('index.html', {root: "./public"}); // load the single view file (angular will handle the page changes on the front-end)
 });
 
+app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.listen(3000);
+console.log("App listening on port 3000");
