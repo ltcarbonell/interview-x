bdf81d66264a175069f732d02740456b93ac7912
Luis (LT) Carbonell <lt.carbonell@gmail.com>
2017-01-11 22:58:08 -0500

    adjust api routes

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ var connection = mysql.createConnection({
   database : 'soccer_app'
 });
 var app = express();
+app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 connection.connect(function(err){
   if(!err) {
@@ -33,13 +37,50 @@ app.get('/api/standings', function(req, res) {
   });
 })
 
+
+app.get('/api/admin', function(req, res) {
+  connection.query('SELECT team_id, name FROM standings', function(err, rows, fields) {
+    if (!err) {
+      res.json(rows);
+    }
+    else {
+      throw err;
+    }
+  });
+})
+
+app.post('/api/admin', function (req, res) {
+  console.log(req.body);
+  var team_id = req.body.team_id;
+  var points = req.body.points;
+  var win = req.body.win;
+  var lose = req.body.lose;
+  var draw = req.body.draw;
+
+  var query = "update standings \
+  set points = points + "+points+", won = won + "+win+", lost = lost + "+lose+", draw = draw + "+draw+"\
+  where team_id = "+team_id+";"
+
+  connection.query(query, function(err, rows, fields) {
+    if (!err) {
+      res.json(rows);
+    }
+    else {
+      throw err;
+    }
+  });
+
+  console.log("Post to homepage")
+  // res.send('POST request to homepage');
+});
+
 app.get("",function(req,res){
   res.sendFile('index.html', {root: "./public"}); // load the single view file (angular will handle the page changes on the front-end)
 });
 
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.get("/admin",function(req,res){
+  res.sendFile('admin.html', {root: "./public"}); // load the single view file (angular will handle the page changes on the front-end)
+});
+
 app.listen(3000);
 console.log("App listening on port 3000");
